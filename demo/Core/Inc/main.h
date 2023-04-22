b565eb2264de504528645b0bde82ae45250798f9
Your Name <you@example.com>
2023-04-22 13:05:45 +0800

    add w25 nor flash driver

--- demo/Core/Inc/main.h
+++ demo/Core/Inc/main.h
@@ -42,7 +42,7 @@ extern "C" {
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
-
+extern SPI_HandleTypeDef hspi4;
 /* USER CODE END EC */
 
 /* Exported macro ------------------------------------------------------------*/
--- demo/src/app.h
+++ demo/src/app.h
@@ -2,6 +2,8 @@
 #define __APP_H
 
 #include "main.h"
+#include "w25qxx.h"
+#include "stdio.h"
 
 #define LED1_ON     HAL_GPIO_WritePin(GPIOD, LED1_Pin, GPIO_PIN_RESET)
 #define LED1_OFF    HAL_GPIO_WritePin(GPIOD, LED1_Pin, GPIO_PIN_SET)
@@ -10,6 +12,9 @@
 #define LED2_ON     HAL_GPIO_WritePin(GPIOD, LED2_Pin, GPIO_PIN_RESET)
 #define LED2_OFF    HAL_GPIO_WritePin(GPIOD, LED2_Pin, GPIO_PIN_SET)
 #define LED2_TOGGLE HAL_GPIO_TogglePin(GPIOD, LED2_Pin)
+
+#define SPI4_CS_HIGH() HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET)
+#define SPI4_CS_LOW()  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET)
 
 void app_loop(void);
 
