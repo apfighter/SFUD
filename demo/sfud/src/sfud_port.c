5514231382f14904160ff297e6c4592742d9cfac
Your Name <you@example.com>
2023-04-27 00:08:58 +0800

    refactor

--- demo/sfud/src/sfud_port.c
+++ demo/sfud/src/sfud_port.c
@@ -32,23 +32,85 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+typedef struct
+{
+    SPI_HandleTypeDef *spix;
+    GPIO_TypeDef      *cs_gpiox;
+    uint32_t           cs_gpio_pin;
+} spi_user_data, *spi_user_data_t;
+
+static spi_user_data spi4 = { .spix = &hspi4, .cs_gpiox = M25X_CS_GPIO_Port, .cs_gpio_pin = M25X_CS_Pin };
+
 static const char *TAG = "SFUD: ";
 
 static char log_buf[256];
 
 void sfud_log_debug(const char *file, const long line, const char *format, ...);
 
-#if 0
+static inline void spi_lock(const sfud_spi *spi)
+{
+    __disable_irq();
+}
+
+static inline void spi_unlock(const sfud_spi *spi)
+{
+    __enable_irq();
+}
+
+static void retry_delay_100us()
+{
+    uint32_t delay = 120;
+    while (delay--);
+}
+
 /**
  * SPI write data then read data
  */
-static sfud_err spi_write_read(const sfud_spi *spi, const uint8_t *write_buf, size_t write_size, uint8_t *read_buf, size_t read_size) {
+static sfud_err spi_write_read(const sfud_spi *spi, const uint8_t *write_buf, size_t write_size, uint8_t *read_buf, size_t read_size)
+{
     sfud_err result = SFUD_SUCCESS;
     uint8_t send_data, read_data;
 
-    /**
-     * add your spi write and read code
-     */
+    spi_user_data_t spi_dev = (spi_user_data_t) spi->user_data;
+    if (!spi_dev)
+    {
+        return SFUD_ERR_NOT_FOUND;
+    }
+
+    if (write_size)
+    {
+        SFUD_ASSERT(write_buf);
+    }
+    if (read_size)
+    {
+        SFUD_ASSERT(read_buf);
+    }
+
+    HAL_GPIO_WritePin(spi_dev->cs_gpiox, spi_dev->cs_gpio_pin, GPIO_PIN_RESET);
+
+    /* 开始读写数据 */
+    for (size_t i = 0; i < write_size + read_size; i++)
+    {
+        /* 先写缓冲区中的数据到 SPI 总线，数据写完后，再写 dummy(0xFF) 到 SPI 总线 */
+        if (i < write_size)
+        {
+            send_data = *write_buf++;
+        }
+        else
+        {
+            send_data = SFUD_DUMMY_DATA;
+        }
+
+        HAL_SPI_TransmitReceive(spi_dev->spix, &send_data, &read_data, 1, 0xFF);
+
+        /* 写缓冲区中的数据发完后，再读取 SPI 总线中的数据到读缓冲区 */
+        if (i >= write_size)
+        {
+            *read_buf++ = read_data;
+        }
+    }
+
+    HAL_GPIO_WritePin(spi_dev->cs_gpiox, spi_dev->cs_gpio_pin, GPIO_PIN_SET);
 
     return result;
 }
@@ -68,7 +130,6 @@ static sfud_err qspi_read(const struct __sfud_spi *spi, uint32_t addr, sfud_qspi
     return result;
 }
 #endif /* SFUD_USING_QSPI */
-#endif
 
 sfud_err sfud_spi_port_init(sfud_flash *flash) {
     sfud_err result = SFUD_SUCCESS;
@@ -91,6 +152,16 @@ sfud_err sfud_spi_port_init(sfud_flash *flash) {
      *    flash->retry.delay = null;
      *    flash->retry.times = 10000; //Required
      */
+
+    flash->spi.wr        = spi_write_read;
+    #ifdef SFUD_USING_QSPI
+    flash->spi.qspi_read = qspi_read;
+    #endif
+    flash->spi.lock      = spi_lock;
+    flash->spi.unlock    = spi_unlock;
+    flash->spi.user_data = &spi4;
+    flash->retry.delay   = retry_delay_100us; /* about 100 microsecond delay */
+    flash->retry.times   = 60 * 10000;        /* adout 60 seconds timeout */
 
     return result;
 }
--- demo/src/app.c
+++ demo/src/app.c
@@ -3,81 +3,6 @@
 #include "string.h"
 #include "sfud.h"
 
-typedef struct
-{
-    SPI_HandleTypeDef *spix;
-    GPIO_TypeDef      *cs_gpiox;
-    uint32_t           cs_gpio_pin;
-} spi_user_data, *spi_user_data_t;
-
-static inline void spi_lock(const sfud_spi *spi)
-{
-    __disable_irq();
-}
-
-static inline void spi_unlock(const sfud_spi *spi)
-{
-    __enable_irq();
-}
-
-/**
- * SPI write data then read data
- */
-static sfud_err spi_write_read(const sfud_spi *spi, const uint8_t *write_buf, size_t write_size, uint8_t *read_buf, size_t read_size)
-{
-    sfud_err result = SFUD_SUCCESS;
-    uint8_t send_data, read_data;
-
-    spi_user_data_t spi_dev = (spi_user_data_t) spi->user_data;
-    if (!spi_dev)
-    {
-        return SFUD_ERR_NOT_FOUND;
-    }
-
-    if (write_size)
-    {
-        SFUD_ASSERT(write_buf);
-    }
-    if (read_size)
-    {
-        SFUD_ASSERT(read_buf);
-    }
-
-    HAL_GPIO_WritePin(spi_dev->cs_gpiox, spi_dev->cs_gpio_pin, GPIO_PIN_RESET);
-
-    /* 开始读写数据 */
-    for (size_t i = 0; i < write_size + read_size; i++)
-    {
-        /* 先写缓冲区中的数据到 SPI 总线，数据写完后，再写 dummy(0xFF) 到 SPI 总线 */
-        if (i < write_size)
-        {
-            send_data = *write_buf++;
-        }
-        else
-        {
-            send_data = SFUD_DUMMY_DATA;
-        }
-
-        HAL_SPI_TransmitReceive(&hspi4, &send_data, &read_data, 1, 0xFF);
-
-        /* 写缓冲区中的数据发完后，再读取 SPI 总线中的数据到读缓冲区 */
-        if (i >= write_size)
-        {
-            *read_buf++ = read_data;
-        }
-    }
-
-    HAL_GPIO_WritePin(spi_dev->cs_gpiox, spi_dev->cs_gpio_pin, GPIO_PIN_SET);
-
-    return result;
-}
-
-static void retry_delay_100us()
-{
-    uint32_t delay = 120;
-    while (delay--);
-}
-
 #define SFUD_DEMO_TEST_BUFFER_SIZE 1024
 static void sfud_demo(uint32_t addr, size_t size, uint8_t *data);
 static uint8_t sfud_demo_test_buf[SFUD_DEMO_TEST_BUFFER_SIZE];
@@ -85,25 +10,10 @@ static uint8_t sfud_demo_test_buf[SFUD_DEMO_TEST_BUFFER_SIZE];
 static uint32_t app_exec_count = 0;
 static uint32_t sfud_inited    = 0;
 
-static spi_user_data spi4 = { .spix = &hspi4, .cs_gpiox = M25X_CS_GPIO_Port, .cs_gpio_pin = M25X_CS_Pin };
-static sfud_flash    flash;
-
 void app_loop(void)
 {
     if (sfud_inited == 0)
     {
-        flash.spi.wr     = spi_write_read;
-        flash.spi.lock   = spi_lock;
-        flash.spi.unlock = spi_unlock;
-#ifdef SFUD_USING_QSPI
-        flash.spi.qspi_read = NULL;
-#endif
-        flash.spi.user_data = &spi4;
-        flash.retry.delay   = retry_delay_100us;  /* about 100 microsecond delay */
-        flash.retry.times   = 60 * 10000;         /* adout 60 seconds timeout */
-
-        sfud_device_ops_register(SFUD_FLASH_MX25L128, &flash);
-
         sfud_inited = 1;
 
         if (sfud_init() == SFUD_SUCCESS) /* SFUD initialize */
